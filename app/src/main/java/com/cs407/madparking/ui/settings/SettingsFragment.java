75e0416449d6e7a9e16763acb07c33e062fb07f3
idkpasswd <donmaking123@gmail.com>
2023-12-01 20:52:03 -0500

    Modify recommended addresses outlay Add save address function to settings

--- app/src/main/java/com/cs407/madparking/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/cs407/madparking/ui/settings/SettingsFragment.java
@@ -1,13 +1,17 @@
 package com.cs407.madparking.ui.settings;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.Switch;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
@@ -17,7 +21,9 @@ import com.cs407.madparking.databinding.FragmentSettingsBinding;
 public class SettingsFragment extends Fragment {
 
     private FragmentSettingsBinding binding;
+    private SharedPreferences sharedPreferences;
 
+    @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         SettingsViewModel settingsViewModel =
@@ -26,7 +32,9 @@ public class SettingsFragment extends Fragment {
         binding = FragmentSettingsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        // Set up the listener for the dark mode switch
+        sharedPreferences = getActivity().getPreferences(Context.MODE_PRIVATE);
+
+        // Dark mode switch
         Switch switchDarkMode = binding.switchDarkMode;
         switchDarkMode.setOnCheckedChangeListener((buttonView, isChecked) -> {
             int mode = isChecked ? AppCompatDelegate.MODE_NIGHT_YES : AppCompatDelegate.MODE_NIGHT_NO;
@@ -34,7 +42,39 @@ public class SettingsFragment extends Fragment {
         });
 
 
+        // Saved address
+        String savedAddress = sharedPreferences.getString("saved_address", "");
+        binding.buttonAddress.setText(savedAddress);
+
+        binding.buttonAddress.setOnClickListener(v -> showEditAddressDialog());
+
         return root;
+    }
+
+    private void showEditAddressDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Edit Address");
+
+        final EditText input = new EditText(requireContext());
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
+        input.setText(sharedPreferences.getString("saved_address", "")); // Prefill with saved address if available
+
+        builder.setView(input);
+
+        builder.setPositiveButton("Save", (dialog, which) -> {
+            String address = input.getText().toString();
+            saveAddressToLocalStorage(address);
+            binding.buttonAddress.setText(address);
+        });
+        builder.setNegativeButton("Cancel", (dialog, which) -> dialog.cancel());
+
+        builder.show();
+    }
+
+    private void saveAddressToLocalStorage(String address) {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("saved_address", address);
+        editor.apply();
     }
 
     @Override
@@ -42,6 +82,4 @@ public class SettingsFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
-
-
 }
